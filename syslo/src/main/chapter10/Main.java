45563d49ef2ab094de6a1b55d0bbe2c495575857
meksula <meksula.karol.93@gmail.com>
2018-09-16 18:38:27 +0200

    Quick Sort algorithm implemented.

--- syslo/src/main/chapter10/Main.java
+++ syslo/src/main/chapter10/Main.java
@@ -1,8 +1,12 @@
-package chapter10;
+package main.chapter10;
+
+import chapter10.MergeSort;
+
+import java.util.Arrays;
 
 /**
  * @author
- * Karol Meksu³a
+ * Karol Meksu?a
  * 27-08-2018
  * */
 
@@ -14,11 +18,21 @@ public class Main {
 		
 		int[] result = MergeSort.merge2(setX, setY);
 		
-		System.out.println("Wynik scalania dwóch ci¹gów uporz¹dkowanych:\n");
+		System.out.println("Wynik scalania dw?ch ci?g?w uporz?dkowanych:\n");
 		
 		for(int i : result) {
 			System.out.print(i + ", ");
 		}
+
+		System.out.println("\n\n********************");
+		System.out.println("QUICK SORT ALGORITHM\n");
+
+		int[] set = {44, 35, 22, 19, 8, 94, 38, 46, 53, 101, 34, 13, 5};
+		System.out.println("BEFORE: " + Arrays.toString(set));
+
+		int[] sortedSet = new QuickSort(set).quickSort();
+
+		System.out.println("AFTER: " + Arrays.toString(sortedSet));
 	}
 	
 }
---
+++ syslo/src/main/chapter10/QuickSort.java
@@ -0,0 +1,79 @@
+package main.chapter10;
+
+import java.util.Random;
+
+/**
+ * @author
+ * Karol Meksuła
+ * 16-09-2018
+ * */
+
+public class QuickSort {
+    private int[] set;
+
+    public QuickSort(int[] set) {
+        this.set = set;
+    }
+
+    /**
+     * Metoda publiczna, rozpocznająca pracę algorytmu rekurencyjnego.
+     * Wywołana metoda sort() z pierwszym elementem tablicy oraz z ostatnim elementem.
+     * */
+    public int[] quickSort() {
+        if (set == null) {
+            throw new IllegalStateException("Nie zainicjalizowano tablicy!");
+        }
+
+        sort(0, set.length - 1);
+        return set;
+    }
+
+    /**
+     * Jeśli pierwszy index jest mniejszy od ostatniego + 1, to */
+    private void sort(int first, int last) {
+        if (first < last + 1) {
+            int index = divide(first, last);
+            sort(first, index - 1);
+            sort(index + 1, last);
+        }
+    }
+
+    /**
+     * Na początku zamieniamy miejscami element o indexie 'first' z losowym elementem z przedziału.
+     * Granicę ustawiamy na first + 1
+     * Iterujemy w pętli od granicy do ostatniego indeksu włącznie:
+     *      Jeżeli element i jest mniejszy od elementu first to zamieniamy miejscami element i z elementem border + 1
+     * Po wyjściu z pętli zamieniamy first z border - 1
+     * */
+    private int divide(int first, int last) {
+        swap(first, getPivot(first, last));
+        int border = first + 1;
+
+        for (int i = border; i <= last; i++) {
+            if (set[i] < set[first]) {
+                swap(i, border++);
+            }
+        }
+
+        swap(first, border - 1);
+        return border - 1;
+    }
+
+    /**
+     * Losowo wyznaczamy środek zbioru spośród jego jeszcze nieposortowanych elementów.
+     * */
+    private int getPivot(int low, int high) {
+        Random rand = new Random();
+        return rand.nextInt((high - low) + 1) + low;
+    }
+
+    /**
+     * Zamienieniamy miejscami elementy o dwóch indeksach
+     * */
+    private void swap(int indexA, int indexB) {
+        int tmp = set[indexA];
+        set[indexA] = set[indexB];
+        set[indexB] = tmp;
+    }
+
+}
